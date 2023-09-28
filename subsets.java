e0987f4e14505d1c90fc29d2e4f838578e1fc2ef
Hinal0101 <hinaljethava112002@gmail.com>
2023-09-28 20:16:50 +0530

    all subsets of 1st n natural nos.

---
+++ subsets.java
@@ -0,0 +1,31 @@
+package Advance_Recursion;
+import java.util.ArrayList;
+
+public class subsets {
+    public static void printSubset(ArrayList<Integer> subset){
+        for(int i=0;i<subset.size();i++){
+            System.out.print(subset.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n,ArrayList<Integer> subset){
+        if(n==0){
+            printSubset(subset);
+            return;
+        }
+        //add hoga
+        subset.add(n);
+        findSubsets(n-1, subset);
+
+        //add nahi hoga
+        subset.remove(subset.size()-1);
+        findSubsets(n-1, subset);
+    }
+
+    public static void main(String args[]){
+        int n=3;
+        ArrayList<Integer> subset=new ArrayList<>();
+        findSubsets(n, subset);
+    }
+}
