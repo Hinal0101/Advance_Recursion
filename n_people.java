511b3a4eb815628e59f3c696ee9aa6de87850696
Hinal0101 <hinaljethava112002@gmail.com>
2023-09-28 20:07:28 +0530

    no. of ways you can invite n people(single/pairs)

---
+++ n_people.java
@@ -0,0 +1,23 @@
+package Advance_Recursion;
+
+public class n_people {
+    public static int callGuests(int n){
+        if(n<=1){
+            return 1;
+        }
+
+        //single
+        int ways1=callGuests(n-1);
+
+        //pairs
+        int ways2=(n-1)*callGuests(n-2);
+
+        return ways1+ways2;
+    }
+
+    public static void main(String args[]){
+        int n=4;
+        System.out.println(callGuests(n));
+
+    }
+}
